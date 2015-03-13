8a2b270ecdef38f79ddb5be659b3d4403ab2170d
michael <michaelwsx@gmail.com>
2015-03-12 20:00:22 -0400

    test

--- app/src/main/java/com/radiance/kamisado/MainActivity.java
+++ app/src/main/java/com/radiance/kamisado/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
