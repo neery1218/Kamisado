066e1e837927c19479288d261d0248d2eba1ebd9
Neerajen <neery1218@gmail.com>
2015-03-31 22:21:19 -0400

    Created GameBoard class which extends View object

---
+++ app/src/main/java/com/radiance/kamisado/GameBoard.java
@@ -0,0 +1,19 @@
+package com.radiance.kamisado;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by neerajen on 31/03/15.
+ */
+public class GameBoard extends View {
+    public GameBoard(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onDraw (Canvas canvas){
+
+    }
+}
