ee36b6ec7471eb99f3116ed41bfd7665ac31e64c
michael <michaelwsx@gmail.com>
2015-04-04 22:37:00 -0400

    commented p1Turn

--- app/src/main/java/com/radiance/kamisado/GameBoard.java
+++ app/src/main/java/com/radiance/kamisado/GameBoard.java
@@ -284,6 +284,7 @@ public class GameBoard extends View {
 
     private void p1Turn(int x, int y){
 
+        //First move of the game
         if(!pieceSelected) {
             for (int i = 0; i < boardDimension; i++) {
                 if (p1[i].getX() == x && p1[i].getY() == y) {
@@ -294,7 +295,10 @@ public class GameBoard extends View {
                 }
             }
         }
+        //When a piece is already selected
         else {
+
+            //For deselecting a piece
             if(firstMove){
                 for(int i = 0; i < boardDimension; i++){
                     if(p1[i].getX() == x && p1[i].getY() == y){
@@ -306,6 +310,7 @@ public class GameBoard extends View {
                 }
 
             }
+
             //If player has no moves available
             if (availMoves.size() == 0) {
                 counter++;
@@ -321,26 +326,32 @@ public class GameBoard extends View {
             //Finds if the clicked square is an available move
             for (int i = 0; i < availMoves.size(); i++) {
                 Point temp = availMoves.get(i);
+
                 if (temp.x == x && temp.y == y) {
+
+                    //Sumo push
                     if (selectedPiece.getRank() > 0 && sumoPushOption != null && temp.x == sumoPushOption.x && temp.y == sumoPushOption.y) {
 
                         //find pieces that are gonna get sumo pushed
                         //make points
+                        //Pushing from the other end so it doesn't get overwritten
                         for (int j = sumoChain; j >= 1; j--) {
+
                             // findPieceAt (x,y+j);
+                            //Push
                             for (int k = 0; k < p2.length; k++) {
                                 if (p2[k].getX() == x && p2[k].getY() == selectedPiece.getY() + j) {
                                     p2[k].setLoc(x, p2[k].getY() + 1);
                                 }
-
-
                             }
                             counter++;
-
                         }
+
+                        //Set location of piece to 1 square in front of it
                         selectedPiece.setLoc(selectedPiece.getX(), selectedPiece.getY() + 1);
                         currColor = board.board8Color[x][sumoPushOption.y];
 
+                        //Find the next piece of other player
                         for (int j = 0; j < boardDimension; j++) {
                             if (p1[j].getColor() == currColor) {
                                 selectedPiece = p1[j];
@@ -348,7 +359,11 @@ public class GameBoard extends View {
                             }
                         }
 
-                    } else {
+                    }
+                    //If sumo push is not committed
+                    else {
+
+                        //Sets piece to mouse clicked location
                         selectedPiece.setLoc(x, y);
                         currColor = board.board8Color[x][y];//next piece color
                         for (int j = 0; j < boardDimension; j++) {
@@ -360,6 +375,7 @@ public class GameBoard extends View {
 
                     }
 
+                    //Increase counter and check if a player has won
                     counter++;
                     win();
                     invalidate();
