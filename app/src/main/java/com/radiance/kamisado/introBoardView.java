5d2ce5997c35a1f732139855d7cfe005c06f928d
Neerajen <neery1218@gmail.com>
2015-05-04 18:35:25 -0400

    added color values of board to xml file

---
+++ app/src/main/java/com/radiance/kamisado/introBoardView.java
@@ -0,0 +1,24 @@
+package com.radiance.kamisado;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by neerajen on 04/05/15.
+ */
+public class introBoardView extends View {
+    public introBoardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void onDraw(Canvas canvas) {
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+
+            }
+        }
+    }
+}
