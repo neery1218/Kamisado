e4084dca9459a0b71651445dcfb39b4417bd3e66
Neerajen <neery1218@gmail.com>
2015-05-10 21:17:57 -0400

    disabled options menu, added OptionsDisplay, which will display and control all gamePlay related options

---
+++ app/src/main/java/com/radiance/kamisado/OptionsDisplay.java
@@ -0,0 +1,16 @@
+package com.radiance.kamisado;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by neerajen on 10/05/15.
+ */
+public class OptionsDisplay extends LinearLayout {//used for all gamePlay related options to the user,
+
+    //such as undo, scoreView, surrender, approving undo's
+    public OptionsDisplay(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
