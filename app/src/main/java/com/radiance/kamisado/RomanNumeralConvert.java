60c864b7e28b5ca3c56f0d22548f8b73a5a44520
Michael <michaelwsx@gmail.com>
2015-06-20 16:36:16 -0400

    fonts are now properly fixed

--- app/src/main/java/com/radiance/kamisado/RomanNumeralConvert.java
+++ app/src/main/java/com/radiance/kamisado/RomanNumeralConvert.java
@@ -7,21 +7,18 @@ abstract class RomanNumeralConvert {
 
     public  static String convertToRomanNumerals(int num){
         String s = "";
-        int i = num % 5, v = num - i;
-        if(v == 1)
-            s += "v";
-        switch(i){
-            case 0: s += ""; break;
+        switch(num){
+            case 0: s += "-"; break;
             case 1: s += "I"; break;
             case 2: s += "II"; break;
             case 3: s += "III"; break;
             case 4: s += "IV"; break;
+            case 5: s += "V"; break;
+            case 6: s += "VI"; break;
+            case 7: s += "VII"; break;
+            case 8: s += "VIII"; break;
+            case 9: s += "IX"; break;
         }
-
-        if(num == 9)
-            s = "IX";
-        else if(num == 0)
-            s = "-";
 
         return s;
     }
